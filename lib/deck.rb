ac25b5f1730f6f86214096c5beb6b36de8f92385
Nick Willett-Jeffries & Emil Lee <nick+emil@appacademy.io>
2013-11-01 14:16:39 -0400

    Add deck class with #collect_suit
    Deck#collect_suit is a convenience function to gather all
    cards of a single suit in the deck.

---
+++ lib/deck.rb
@@ -0,0 +1,24 @@
+require_relative 'card'
+
+class Deck
+
+  def initialize
+    build_deck
+  end
+
+  def build_deck
+    @cards = Array.new(13) { Card.new(:hearts, "10") }
+    @cards << Card.new(:spades, "7")
+  end
+
+  def shuffle
+  end
+
+  def deal_card
+  end
+
+  def collect_suit(suit)
+    @cards.select { |card| card.suit == suit }
+  end
+
+end
