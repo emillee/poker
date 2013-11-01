ae390edd1e1c7d9ac4862653f474f43426fdc66f
Nick Willett-Jeffries & Emil Lee <nick+emil@appacademy.io>
2013-11-01 17:20:51 -0400

    Define basic outline of hand spec

---
+++ spec/hand_spec.rb
@@ -0,0 +1,25 @@
+require 'rspec'
+require 'hand'
+
+describe Hand do
+
+  describe "#best_hand" do
+
+    it "detects a straight flush"
+
+    it "detects a four of a kind"
+
+    it "detects a flush"
+
+    it "detects a straight"
+
+    it "detects a three of a kind"
+
+    it "detects a two pair"
+
+    it "detects a one pair"
+
+    it "detects a high card"
+
+  end
+end
