9aed850d0501ef56487d26e49962ed1b1ab7543c
Nick Willett-Jeffries & Emil Lee <nick+emil@appacademy.io>
2013-11-01 14:21:26 -0400

    Extend deck specs for #build_deck function

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -18,16 +18,41 @@ describe Deck do
     end
   end
 
+  describe "#collect_rank" do
+    it "should return 4 kings in a new deck" do
+      expect(deck.collect_rank('K').count).to eq(4)
+    end
+
+  end
+
   describe "#build_deck" do
-    it "should create a deck with a 52 cards"
+    it "should create a deck with a 52 cards" do
+      expect(deck.cards.count).to eq(52)
+    end
 
-    it "should create a deck with 13 spades"
+    it "should create a deck with 13 spades" do
+      expect(deck.collect_suit(:spades).count).to eq(13)
+    end
 
-    it "should create a deck with 13 clubs"
+    it "should create a deck with 13 hearts" do
+      expect(deck.collect_suit(:hearts).count).to eq(13)
+    end
 
-    it "should create a deck with 13 hearts"
+    it "should create a deck with 13 clubs" do
+      expect(deck.collect_suit(:clubs).count).to eq(13)
+    end
 
-    it "should create a deck with 13 diamonds"
+    it "should create a deck with 13 diamonds" do
+      expect(deck.collect_suit(:diamonds).count).to eq(13)
+    end
 
-    it "should have four kings"
+    it "should create a deck with four kings" do
+      expect(deck.collect_rank('K').count).to eq(4)
+    end
+  end
+
 end
+
+
+
+
